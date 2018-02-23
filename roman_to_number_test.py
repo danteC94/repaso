d6e9499865bd07445e15e6355a7284fcd2ce9b1d
danteC94 <dacanizo94@gmail.com>
2018-02-23 19:31:24 -0300

    segundo commit

---
+++ roman_to_number_test.py
@@ -0,0 +1,9 @@
+import unittest
+from roman_to_number import convert_roman_to_decimal
+
+class TestRomanToNumber(unittest.TestCase):
+    def test_roman_to_decimal_one(self):
+        resultado = convert_roman_to_decimal("I")
+        self.assertEqual(resultado, 1)
+if __name__ == "__main__":
+    unittest.main()
