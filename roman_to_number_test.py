116bcc21bf6182374c9a3ec18665285cb30aac82
danteC94 <dacanizo94@gmail.com>
2018-02-26 17:11:08 -0300

    squas_commits
    prueba git rebae
    
    preuba git rebase 2

--- roman_to_number_test.py
+++ roman_to_number_test.py
@@ -1,9 +1,12 @@
 import unittest
 from roman_to_number import convert_roman_to_decimal
 
+
 class TestRomanToNumber(unittest.TestCase):
     def test_roman_to_decimal_one(self):
         resultado = convert_roman_to_decimal("I")
         self.assertEqual(resultado, 1)
+
+
 if __name__ == "__main__":
     unittest.main()
